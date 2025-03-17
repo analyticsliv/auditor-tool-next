cbe50277cba23da09ec24bc4e6308dced726aae4
pruthviraj0306 <pruthvirajsinh.rathod@analyticsliv.com>
2025-03-17 17:33:17 +0530

    added moment library to get start and end date accordingly

--- app/utils/callApis.js
+++ app/utils/callApis.js
@@ -1,16 +1,23 @@
+import moment from "moment";
 import { fetchAuditData, reportEndApiCall } from "./endApi";
+
+const today = moment();
+const yesterday = moment().subtract(1, 'days');
+const startDate = moment().subtract(30, 'days');
+const formattedStartDate = startDate.format('YYYY-MM-DD');
+const formattedEndDate = yesterday.format('YYYY-MM-DD');
 
 const endapiall = {
     "dimensions": [{ "name": "streamId" }, { "name": "streamName" }],
     "metrics": [{ "name": "activeUsers" }],
-    "dateRanges": [{ "startDate": '2025-02-15', "endDate": '2025-03-17' }],
+    "dateRanges": [{ "startDate": formattedStartDate, "endDate": formattedEndDate }],
     "keepEmptyRows": true
 };
 
 const usersDetails = {
     "dimensions": [{ "name": "hostName" }],
     "metrics": [{ "name": "totalUsers" }, { "name": "sessions" }],
-    "dateRanges": [{ "startDate": '2025-02-15', "endDate": '2025-03-17' }],
+    "dateRanges": [{ "startDate": formattedStartDate, "endDate": formattedEndDate }],
     "keepEmptyRows": true
 };
 
