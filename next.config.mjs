923054e6b4fd72e8c313904b330635a059aa9931
vermaAtul1520 <atulverma1520@gmail.com>
2025-06-09 17:26:08 +0530

    deployment configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['lh3.googleusercontent.com'], // Add Google as a trusted domain
-    },
-  };
-  
-  export default nextConfig;  
+  output: 'standalone',
+  experimental: {
+    outputFileTracingRoot: undefined,
+  },
+  images: {
+    domains: ['lh3.googleusercontent.com'], // Add Google as a trusted domain
+  },
+};
+
+export default nextConfig;  
