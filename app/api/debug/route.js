fbc7d0ad728dbb8acbe4a26a7e7cbd66177fc4a9
vermaAtul1520 <atulverma1520@gmail.com>
2025-06-11 17:45:48 +0530

    added a debug endpoit

---
+++ app/api/debug/route.js
@@ -0,0 +1,14 @@
+export async function GET() {
+    return Response.json({
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        // NODE_ENV: process.env.NODE_ENV,
+        allEnvKeys: Object.keys(process.env).filter(key =>
+            key.includes('NEXT') || key.includes('GOOGLE')
+        ),
+        headers: {
+            host: headers().get('host'),
+            'x-forwarded-host': headers().get('x-forwarded-host'),
+            'x-forwarded-proto': headers().get('x-forwarded-proto'),
+        }
+    });
+}
