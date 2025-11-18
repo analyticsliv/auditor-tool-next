660f71e3880863573788966c3709a49bb9e5b759
Pruthviraj <pruthvirajsinh.rathod@analyticsliv.com>
2025-11-18 12:06:22 +0530

    component list based on isecommerce or not to prevent rendering ecommerce components

--- app/utils/componentList.js
+++ app/utils/componentList.js
@@ -1,4 +1,3 @@
-
 import DataStreams from '../Components/dataStreams';
 import GeneralConfig from '../Components/generalConfig';
 import DataCollectionRetention from '../Components/dataCollectionRetention';
@@ -17,6 +16,40 @@ import EcomItemDetails from '../Components/ecomItemDetails';
 import Acquisitions from '../Components/acquisitions';
 import CustomDimensionMetrics from '../Components/customDimensionMetrics';
 import DownloadPdf from '../Components/downloadPdf';
+import { useAccountStore } from '../store/useAccountStore';
+
+// Function that returns components based on isEcommerce flag
+export const getComponentsList = () => {
+    const { isEcommerce } = useAccountStore.getState();
+    console.log("isecommerce-----", isEcommerce)
+    const baseComponents = [
+        DataStreams,
+        GeneralConfig,
+        DataCollectionRetention,
+        AttributionSetting,
+        ActiveDomains,
+        ProductLinks,
+        CoreMetrics,
+        EngagementRate,
+        EngagementMetrics,
+        EventsTracking,
+        KeyEvents,
+        ConversionAnomaly,
+    ];
+
+    // Only include ecommerce components if isEcommerce is true
+    const ecommerceComponents = isEcommerce
+        ? [EcommerceTracking, EcomFunnels, EcomItemDetails]
+        : [];
+
+    const endComponents = [
+        Acquisitions,
+        CustomDimensionMetrics,
+        DownloadPdf,
+    ];
+
+    return [...baseComponents, ...ecommerceComponents, ...endComponents];
+};
 
 export default [
     DataStreams,
